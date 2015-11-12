cfffb35f669c0fb8dc65bd61506fbce0e620eccb
Marc Reicher <msreicher@gmail.com>
2015-11-12 12:35:50 -0800

    Added fireproof and updated server to include promises

--- server/server.js
+++ server/server.js
@@ -5,9 +5,12 @@ var request = require('request');
 var fire = require('firebase');
 var app = express();
 var Promise = require('bluebird');
+var Fireproof = require('fireproof')
+Fireproof.bless(Promise);
 
 
-var ref = new fire('https://rooftopapp.firebaseio.com/')
+var ref = new fire('https://rooftopapp.firebaseio.com/');
+var fireproof = new Fireproof(ref);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -15,45 +18,24 @@ app.use(bodyParser.json());
 //update path names based on where index.html file is located
 // app.use(express.static(__dirname + '/..'));
 app.get('/', function(req, res) {
-	console.log('test');
-	// ref.on("value", function(snapshot) {
- //  		console.log(snapshot.val());
-	// }, function (errorObject) {
- //  		console.log("The read failed: " + errorObject.code);
-	// });
-	res.sendFile(path.join(__dirname + '/../index.html'));
+  console.log('test');
+  res.sendFile(path.join(__dirname + '/../index.html'));
 })
 
-var dbMiddleware = function(req, res, next) {
-	// ref.orderByChild('location/postal_code').equalTo('90401').on('child_added', function(snapshot) {
-	// 	console.log(snapshot.val().name);
-	// 	arr.push(snapshot.val().name);
-	// })
-	res.bars = []
-	var dbQuery = function () {
-			ref.orderByChild('location/city').equalTo('Los Angeles').on('child_added', function(snapshot) {
-				// console.log(snapshot.val().name);
-				res.bars.push(snapshot.val().name);
-			})
-
-
-	}
-	console.log(dbQuery, 'FUNCTION');
-	dbQuery().then(function() {
-		next();
-	})
-	
-}
-
-app.post('/', dbMiddleware, function(req, res) {
-	// console.log(res.bars);
-	res.send('test')
-
-	
-	// .then(
-	// 	res.send(JSON.stringify(arr));
-
-	// 	)
+app.post('/', function(req, res) {
+   // console.log(res.bars);
+  res.bars = []
+  // console.log(req.body);
+  // console.log(JSON.parse(req.body));
+  fireproof.orderByChild('location/city').equalTo('Los Angeles').on('child_added', function(snapshot) {
+    // console.log(snapshot.val().name);
+    res.bars.push(snapshot.val().name);
+  }).then(function() {
+    console.log(res.bars)
+    res.send(res.bars);
+  }, function(err) {
+    console.error(err);
+  })
 
 })
 
