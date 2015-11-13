a016b31212211eeb46188fe50b2dbbb9fcafd7a9
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2015-11-13 12:08:19 -0800

    fix server to handle query parameters

--- server/queries.js
+++ server/queries.js
@@ -11,16 +11,16 @@ exports.getList = function(req, res, next) {
 
 	if (req.body.city) {
 		// console.log('got a city search for ' + req.body.city);
-		queryDB(req, res, next, 'location/city');
+		queryDB(req, res, next, 'location/city', req.body.city);
 	} else if (req.body.zipCode) {
 		// console.log('got a zip search for ' + req.body.zipCode);
-		queryDB(req, res, next, 'location/postal_code');
+		queryDB(req, res, next, 'location/postal_code', req.body.zipCode);
 	}
 };
 
-function queryDB(req, res, next, searchParam) {
+function queryDB(req, res, next, searchParam, queryParam) {
 		fireproof.orderByChild(searchParam)
-		.equalTo(req.body.zipCode)
+		.equalTo(queryParam)
 		.on('child_added', function(snapshot) {
 			res.bars.push(snapshot.val());
   	})
